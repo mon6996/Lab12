7bddcf8ec3d8ec919616d8674efb3d3c1d33a549
M <thanaporn_chan@cmu.ac.th>
2021-12-19 01:48:19 +0700

    12_1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    string enter, grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int j = 0; j < 3; j++)
+        getline(cin,enter);
+    srand(time(0));
+    i = rand()%9;
+    cout << "You will get " << grade[i] << " in this 261102.";
+    return 0;
+}
